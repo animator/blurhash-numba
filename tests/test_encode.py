2cff6d7cae732eba0b1b9880a4230b7f604607f2
Ankit Mahato <ankmahato@gmail.com>
2020-09-25 06:22:41 +0530

    Fix argument names

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -23,20 +23,20 @@ def test_decode():
 def test_asymmetric():
     image = PIL.Image.open(os.path.join(base_path, "cool_cat.jpg"))
     image_array = np.array(image.convert("RGB"), dtype=np.float)
-    blur_hash = encode(image_array, components_x = 2, components_y = 8)
+    blur_hash = encode(image_array, x_components = 2, y_components = 8)
     assert blur_hash == "%BMOZfK1BBNG2skqs9n4?HvgJ.Nav}J-$%sm"
     
     decoded_image = decode(blur_hash, 32, 32)
     assert np.sum(np.var(decoded_image, axis = 0)) > np.sum(np.var(decoded_image, axis = 1))
 
-    blur_hash = encode(image_array, components_x = 8, components_y = 2)
+    blur_hash = encode(image_array, x_components = 8, y_components = 2)
     decoded_image = decode(blur_hash, 32, 32)
     assert np.sum(np.var(decoded_image, axis = 0)) < np.sum(np.var(decoded_image, axis = 1))
 
 def test_components():
     image = PIL.Image.open(os.path.join(base_path, "cool_cat.jpg"))
     image_array = np.array(image.convert("RGB"), dtype=np.float)
-    blur_hash = encode(image_array, components_x = 8, components_y = 3)
+    blur_hash = encode(image_array, x_components = 8, y_components = 3)
     size_x, size_y = components(blur_hash)
     assert size_x == 8
     assert size_y == 3
@@ -44,7 +44,7 @@ def test_components():
 def test_linear_dc_only():
     image = PIL.Image.open(os.path.join(base_path, "cool_cat.jpg"))
     linearish_image = np.array(image.convert("RGB"), dtype=np.float) / 255.0
-    blur_hash = encode(linearish_image, components_x = 1, components_y = 1, linear = True)
+    blur_hash = encode(linearish_image, x_components = 1, y_components = 1, linear = True)
     avg_color = decode(blur_hash, 1, 1, linear = True)
     reference_avg_color = np.mean(linearish_image.reshape(linearish_image.shape[0] * linearish_image.shape[1], -1), 0)
     assert np.sum(np.abs(avg_color - reference_avg_color)) < 0.01
@@ -59,13 +59,13 @@ def test_invalid_parameters():
          decode("UBMOZfK1GG%LBBNG", 32, 32)
          
     with pytest.raises(ValueError):
-        encode(image, components_x = 0, components_y = 1)
+        encode(image, x_components = 0, y_components = 1)
     
     with pytest.raises(ValueError):
-        encode(image, components_x = 1, components_y = 0)    
+        encode(image, x_components = 1, y_components = 0)    
     
     with pytest.raises(ValueError):
-        encode(image, components_x = 1, components_y = 10)
+        encode(image, x_components = 1, y_components = 10)
       
     with pytest.raises(ValueError):
-        encode(image, components_x = 10, components_y = 1)
+        encode(image, x_components = 10, y_components = 1)
